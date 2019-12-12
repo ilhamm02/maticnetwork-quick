d77abb09423a9bd0a85f2b572dbb04bf13d87bdd
Ilham Maulana <ilhamaulana24@gmail.com>
2019-12-12 12:15:37 +0700

    Update install_matic.sh

--- install_matic.sh
+++ install_matic.sh
@@ -12,7 +12,7 @@ echo -e "${GREEN} Installing Golang (1.13.3).... ${DF}"
 
 wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
 
-sudo tar -xvf go1.13.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.13.5.linux-amd64.tar.gz
 
 mkdir go
 
